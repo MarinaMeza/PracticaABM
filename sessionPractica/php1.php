88d5422aec1ee11d48e09922dea4f4792e2c6152
MarinaMeza <kaiodelnorte@gmail.com>
2017-05-22 15:27:25 -0300

    Practica abm commit 8 ejemplo ajax

--- sessionPractica/php1.php
+++ sessionPractica/php1.php
@@ -2,7 +2,7 @@
 
     session_start();//permite trabajar con sesion en la pagina
 
-    $_SESSION["palabra"] = "cualquier cosa";
+    $_SESSION["palabra"] = "cualquier cosa";//palabra es el usuario en este caso
 
     echo "<a href='html1.html'>Ir a html</a>"
 ?>
